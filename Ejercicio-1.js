5aa5ebe465728a600c5cd945650c0613be289700
Santiago <santiago@Santiagos-MacBook-Air.local>
2026-09-14 17:59:13 -0500

    se hizo el primer ejercicio de JavaScript

---
+++ Ejercicio-1.js
@@ -0,0 +1,20 @@
+
+function calculadora(num, num2, operador){
+    switch (operador){
+        case "+":
+            console.log("El resultado es: " + (parseInt(num) + parseInt(num2)));
+            break;
+        case "-":
+            console.log("El resultado es: " + (parseInt(num) - parseInt(num2)));
+            break;
+        case "*":
+            console.log("El resultado es: " + (parseInt(num) * parseInt(num2)));
+            break;
+        case "/":
+            console.log("El resultado es: " + (parseInt(num) / parseInt(num2)));
+            break;
+        default:
+            console.log("Operador no válido");
+    }
+}
+calculadora(10, 5, "+");

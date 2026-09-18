48b7c720dc8ba4e634e3be774cd86f8088cea304
santiago07gl <santiago.gonzalez07@uceva.edu.co>
2026-09-18 12:33:01 -0500

    feat: crear archivo ejercicio-3.ts y se realiza correctamente el ejercicio

---
+++ ejercicio-3.ts
@@ -0,0 +1,23 @@
+type Producto = {
+    id: number
+    nombre: string
+    precio: number
+    disponible: boolean
+}
+
+let producto1: Producto = {
+    id: 1201,
+    nombre: 'Manzana',
+    precio: 5000,
+    disponible: true
+}
+
+let producto2: Producto = {
+    id: 1202,
+    nombre: 'papa',
+    precio: 1200,
+    disponible: false
+}
+
+console.log(producto1);
+console.log(producto2);
